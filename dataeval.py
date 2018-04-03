136f05f6da6237a4d4ae286c583ab7d6b9cca34e
Maisy Wilcox <maisy.wilcox@maisywilcox.com>
2018-04-03 10:24:39 -0400

    Updated from Brython Server: 4/3/2018 10:24:38 AM

--- dataeval.py
+++ dataeval.py
@@ -59,7 +59,7 @@ def LinReg(list,start,interval):
     x2=[j**2 for j in x]
     n=len(list)
     m=((n*sum(xy))-(sum(x)*sum(y)))/((n*sum(x2))-(sum(x)**2))
-    b=((sum(y)*sum(x2))-(sum(x)*sum(xy))/((n*sum(x2))-(sum(x)**2))
+    b=((sum(y)*sum(x2))-(sum(x)*sum(xy)))/((n*sum(x2))-(sum(x)**2))
     ans=[m,b]
     return(ans)
 
