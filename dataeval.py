99d3af30b1bbe417ddd0f4fc00a71fd821e249a2
Maisy Wilcox <maisy.wilcox@maisywilcox.com>
2018-04-03 09:30:34 -0400

    Update dataeval.py

--- dataeval.py
+++ dataeval.py
@@ -6,9 +6,9 @@ Experiment vars which could be input but are just written into the code are defi
 
 smoothing function was provided in the scipy numpy handbook and is not my own work
 """
-HOTWATERMASS=[47.566,39.409,50.068,45.215,50.995,51.541]
-COLDWATERMASS=[50.240,49.317,50.120,48.420,59.995,49.905]
-HOTWATERTEMP=[95,93,94,92,95,89]
+WATERMASS=[]
+SALTMASS=[]
+DATALEN=150
 
 
 def smooth(list,degree=3):
@@ -85,126 +85,3 @@ def convert(iteration):
         funclist.append(float(ls[0]))
     functrial.close()
     return(funclist)
-
-
-def SetDivide(list):
-    set0=[]
-    set1=[]
-    set2=[]
-    set3=[]
-
-    ##set0
-    set0.append(list[0])
-    i = 1
-    stop = 0
-    while stop == 0:
-        medbound = mean(list)
-        limit = 1
-        lowbound = medbound - limit
-        highbound = medbound + limit
-        temptest=list[i]
-        if temptest >= lowbound and temptest <= highbound:
-            set0.append(temptest)
-        else:
-            stop = 1
-            setof0=i-1
-        i += 1
-    stop = 0
-
-    ##set2
-
-
-    ##set2
-    end = 1000
-    i=len(list)-end
-    while i < len(list):
-        set3.append(list[i])
-        i += 1
-
-    i=(len(list)-end)-1
-
-    while stop == 0:
-        reg=LinReg(set3,i)
-        m=reg[0]
-        b=reg[1]
-        yest= (m*i)+b
-        sigma = (yest - list[i])**2
-        if sigma < 16:
-            set3.insert(0,list[i])
-        else:
-            stop = 1
-            setof2 = i + 1
-        i = i-1
-    stop = 0
-
-    ##set3
-    setof3=len(list)
-
-    ##sumative
-    full=[setof0,setof1,setof2,setof3]
-    return(full)
-
-
-def CalCalc(list,breakpts,iteration):
-    lin0lis=[]
-    lin1lis=[]
-    lin2lis=[]
-    i=0
-    while i < breakpts[0]:
-        lin0lis.append(list[i])
-        i += 1
-    i=breakpts[0]
-    while i < breakpts[1]:
-        lin1lis.append(list[i])
-        i += 1
-    i=breakpts[2]
-    while i < breakpts[3]:
-        lin2lis.append(list[i])
-        i += 1
-    lin1=LinReg(lin1lis)
-    lin2=LinReg(lin2lis)
-    peak=LineInt(lin1,lin2)
-    deltat=abs(mean(lin0lis)-peak)
-    deltathot=abs(HOTWATERTEMP[iteration]-peak)
-    deltatcold=abs(mean(lin0lis)-peak)
-    deltaqhot=HOTWATERMASS[iteration] * 4.179 * deltathot
-    deltaqcold=COLDWATERMASS[iteration] * 4.179 * deltatcold
-    calcon=(deltaqhot-deltaqcold)/deltat
-    return([mean(lin0lis),peak,calcon])
-
-
-##work
-
-Trials=[]
-i=1
-
-while i < 7:
-    Trials.append(convert(i))
-    i += 1
-
-i=0
-SmoothTrials=[]
-
-while i < len(Trials):
-    SmoothTrials.append(smooth(Trials[i]))
-    i += 1
-
-i=0
-BreakPts=[]
-while i < len(SmoothTrials):
-    BreakPts.append(SetDivide(SmoothTrials[i]))
-    i += 1
-"""
-i=0
-calcon=[]
-while i < len(SmoothTrials):
-    calcon.append(CalCalc(SmoothTrials[i],BreakPts[i],i))
-    i += 1
-
-print(calcon)
-"""
-
-##testing
-test=SetDivide(SmoothTrials[3])
-print(test[0])
-print(test[3][0])
