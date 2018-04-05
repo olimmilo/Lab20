d21c82a34c142d60fc6639caace5df70862232ce
Maisy Wilcox <maisy.wilcox@maisywilcox.com>
2018-04-05 10:16:47 -0400

    Updated from Brython Server: 4/5/2018 10:16:47 AM

--- dataeval.py
+++ dataeval.py
@@ -16,7 +16,7 @@ DATALEN=175
 INTERVALS=[1,1,1,1,2,2]
 
 ROWLABELS=["Minimum Temperature","Maximum Temperature","Change in Temperature","Enthalpy"]
-COLLUMNLABELS=["___","Nickel Chloride","Sodium Nitrate","Aluminium Nitrate"]
+COLLUMNLABELS=["","Nickel Chloride","Sodium Nitrate","Aluminium Nitrate"]
 
 ##defines functions
 
